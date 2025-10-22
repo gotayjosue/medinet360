a745105096b3146c9eb1b2b7fa71ff8feecbd968
gotayjosue <gotayjosue@gmail.com>
2025-10-21 23:47:23 -0600

    Home page almost done

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,11 @@
 const currentYear = new Date().getFullYear();
 document.getElementById('currentYear').textContent = currentYear;
 
+// Toggle navigation menu
+const menuButton = document.getElementById('menuButton');
+const navMenu = document.getElementById('navigation');
+
+menuButton.addEventListener('click', () => {
+    navMenu.classList.toggle('open');
+    menuButton.classList.toggle('open');
+});
