ab63454d0a0278b7a7dafd49e6d0713f4bbd5dc9
gotayjosue <gotayjosue@gmail.com>
2025-10-22 11:37:36 -0600

    Home page finished

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,14 @@ document.getElementById('currentYear').textContent = currentYear;
 const menuButton = document.getElementById('menuButton');
 const navMenu = document.getElementById('navigation');
 
+// Close the menu when resizing the window to a width greater than 768px
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+        navMenu.classList.remove('open');
+        menuButton.classList.remove('open');
+    }
+});
+
 menuButton.addEventListener('click', () => {
     navMenu.classList.toggle('open');
     menuButton.classList.toggle('open');
