e0de9d42e3bb5fcdea63a10fd53718bb31dc341a
gotayjosue <gotayjosue@gmail.com>
2025-11-27 11:14:35 -0600

    Changes made to folders path

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,22 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "src/",
   build: {
-    outDir: "../dist",
+    outDir: "dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
-        faq: resolve(__dirname, "src/faq.html"),
-        pricing: resolve(__dirname, "src/pricing.html"),
-        signIn: resolve(__dirname, "src/signIn.html"),
-        signUp: resolve(__dirname, "src/signUp.html"),
-        dashboardHome: resolve(__dirname, "src/dashboard/home.html"),
-        dashboardPatients: resolve(__dirname, "src/dashboard/patients.html"),
-        dashboardAppointments: resolve(__dirname, "src/dashboard/appointments.html"),
-        dashboardAccount: resolve(__dirname, "src/dashboard/account.html"),
-        dashboardReports: resolve(__dirname, "src/dashboard/reports.html"),
-        patientsDetails: resolve(__dirname, "src/dashboard/patientDetails.html"),
-        dashboardAppointmentsCalendar: resolve(__dirname, "src/dashboard/appointmentsCalendar.html"),
+        main: resolve(__dirname, "index.html"),
+        faq: resolve(__dirname, "faq.html"),
+        pricing: resolve(__dirname, "pricing.html"),
+        signIn: resolve(__dirname, "signIn.html"),
+        signUp: resolve(__dirname, "signUp.html"),
+        dashboardHome: resolve(__dirname, "dashboard/home.html"),
+        dashboardPatients: resolve(__dirname, "dashboard/patients.html"),
+        dashboardAppointments: resolve(__dirname, "dashboard/appointments.html"),
+        dashboardAccount: resolve(__dirname, "dashboard/account.html"),
+        dashboardReports: resolve(__dirname, "dashboard/reports.html"),
+        patientsDetails: resolve(__dirname, "dashboard/patientDetails.html"),
+        dashboardAppointmentsCalendar: resolve(__dirname, "dashboard/appointmentsCalendar.html"),
       },
     },
   },
